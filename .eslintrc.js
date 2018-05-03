bc660ef56cc6b33d3afe6729bd5fb895a2293a6a
Vanja Gavric <vanja@gavric.org>
2018-05-03 15:52:05 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: [
+    'standard',
+  ],
+  rules: {
+    complexity: ['error', 10],
+    'object-curly-spacing': ['error', 'always'],
+    'max-len': ['error', {
+      code: 80,
+      ignoreComments: true,
+      ignoreStrings: true,
+      ignoreUrls: true,
+      ignoreTemplateLiterals: true,
+    }],
+    'no-console': 'error'
+  }
+};
